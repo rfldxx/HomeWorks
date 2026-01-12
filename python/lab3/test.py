f4b5bb43e451715f5afb260b32f42799fe765690
rfldxx <166624946+rfldxx@users.noreply.github.com>
2026-01-12 11:29:25 +0300

    Update test.py

--- python/lab3/test.py
+++ python/lab3/test.py
@@ -1,7 +1,3 @@
-# ------------------------------------------------------------------------------
-# Тесты
-# ------------------------------------------------------------------------------
-
 # from gen_bin_tree import gen_bin_tree
 
 from typing import Callable, Any, List, Tuple
@@ -10,7 +6,7 @@ import unittest
 
 
 class TestGenBinTree(unittest.TestCase):
-    def presetFunction(self, lst: List[int], updater: Callable[[int, Any], int] = lambda x, _: x) -> Tuple[Callable[[Any], int], Callable[[Any], None], Callable[[], bool]]:
+    def presetFunction(self, lst: List[int], updater: Callable[[int, Any], int] = lambda x, _: x+1) -> Tuple[Callable[[Any], int], Callable[[Any], None], Callable[[], bool]]:
         """
         Создание функции (`f`) возвращающей элемент из списка lst на основе внутреннего индекса `i`, без повторного доступа к индексу;
         и оберток (`update`, `is_ended`) для контроля этого идекса `i` и проверки были ли все значения списка lst посещены.
@@ -21,9 +17,9 @@ class TestGenBinTree(unittest.TestCase):
 
         Returns:
             A tuple containing:
-            - f:        Функция которая, внезависимости от аргумента, возвращает lst[i], где `i` - внутренний индекс.
-            - update:   Функция обновляющая внутренний индекс `i` на основе переданного аргумента.
-            - is_ended: Булевая функция возвращающая True, если все значения списка lst были посещены.
+                f:        Функция которая, внезависимости от аргумента, возвращает lst[i], где `i` - внутренний индекс.
+                update:   Функция обновляющая внутренний индекс `i` на основе переданного аргумента.
+                is_ended: Булевая функция возвращающая True, если все значения списка lst были посещены.
 
         Raises:
             IndexError: "re-accessing the index: {i}"         - функция `f` пытается повтороно вернуть lst[i].
@@ -57,9 +53,14 @@ class TestGenBinTree(unittest.TestCase):
 
     def test_by_generating_f_g(self) -> None:
         """
-        Рекурсивная проверка, что дерево, сгеннерированое по функциям f и g, действительно имеет эти значения в узлах.
-        Функция f - возвращает последовательно чётные значения (начиная с 0), функция g - нечётные.
+        Проверка, что дерево согласуется с обходом в глубину.
+
+        Рекурсивная проверка, что дерево, сгеннерированое по preset-функциям f и g, действительно имеет эти значения в узлах.
+        Для этого создаются копии этих preset-функций и значения в дереве сравниваются с значениями скопированных функций в реккурсивном обходе.
         Также проверяется что каждая функция (f и g) вызывается ровно (2^height - 1) раз.
+
+        В тестировании функция f возвращает последовательно чётные значения (начиная с 0), функция g - нечётные.
+        Тестируются деревья с высотами из списка cheked_height.
         """
         cheked_height = range(0, 10+1)
 
@@ -68,7 +69,7 @@ class TestGenBinTree(unittest.TestCase):
             Обёртка над f и update_f - пригодная для использования в тестируемой функции gen_bin_tree.
 
             Returns:
-                Функция `F(x)` - функция возвращающая значение f(x) и внешним эффектом update_f().
+                Функция `F(x)` - функция возвращающая значение f(x) и внешним эффектом в виде вызова update_f().
             """
             def F(x: Any) -> int:
                 result = f(x)
@@ -98,7 +99,7 @@ class TestGenBinTree(unittest.TestCase):
 
 
         for h in cheked_height:
-            L = 2**h - 1  # Number of function calls for each f_lst or g_lst
+            L = 2**h - 1  # Количество вызовов каждой из функций f и g при создании дерева выстоты h
             f_lst = [2 * i     for i in range(L)]  #   четные значения для f
             g_lst = [2 * i + 1 for i in range(L)]  # нечетные значения для g
 
@@ -111,7 +112,7 @@ class TestGenBinTree(unittest.TestCase):
             self.assertTrue(is_ended_f(), "Not all f() values used in generation")
             self.assertTrue(is_ended_g(), "Not all g() values used in generation")
 
-            # Проверка корректности дерева
+            # создание копий функций f и g
             f1, update_f1, is_ended_f1 = self.presetFunction(f_lst, lambda i, _: i+1)
             g1, update_g1, is_ended_g1 = self.presetFunction(g_lst, lambda i, _: i+1)
 
@@ -119,3 +120,125 @@ class TestGenBinTree(unittest.TestCase):
 
             self.assertTrue(is_ended_f1(), "Not all f() values used in checking")
             self.assertTrue(is_ended_g1(), "Not all g() values used in checking")
+
+
+
+    def test_by_generating_tree_list(self) -> None:
+        """
+        Проверка, что дерево согласуется с обходом в ширину.
+        
+        Рекурсивная проверка, что дерево, сгеннерированое по preset-функциям `*FG_positioned`, действительно имеет эти значения в узлах.
+        Для этого создаются копии этих preset-функций и значения в дереве сравниваются с значениями скопированных функций в реккурсивном обходе.
+        Также проверяется что каждая функция (f и g) вызывается ровно (2^height - 1) раз.
+
+        В тестировании, значения для узлов дерева создаются в виде (0-индексированного) списка `TreeList`.
+        Корень дерева имеет значение `TreeList[0]`, любая вершина под индексом `i` имеет левого потомка под индексом `2i+1`, правого `2i+2`.
+        
+        Функции из `*FG_positioned` выдают значения для левого потомка (`F`) и для правого (`G`) на основе значений из `TreeList`.
+         
+        Тестируются деревья с высотами из списка cheked_height.
+        """
+        cheked_height = range(0, 10+1)
+
+
+        def path_to_indx(position: List[int]) -> int:
+            """Преобразует путь в дереве (последовательность 0/1: переход влево/вправо) в нуль-индексированую позицию в порядке обхода в ширину."""
+            i = 1
+            for e in position:
+                i = 2 * i + e
+            return i - 1
+
+
+        def FG_positioned(h: int, f: Callable[[Any], int], g: Callable[[Any], int], share: Callable[[List[bool]], None] = lambda x : None) -> Tuple[Callable[[int], int], Callable[[int], int]]:
+            """
+            Создает функции `F` и `G` - пригодные для использования в тестируемой функции gen_bin_tree - обёртки над функциями f и g.
+            Такая морока нужна, чтобы переданные в gen_bin_tree функции могли отслеживать на каком шаге выполнения рекурсии они находятся.
+
+            Функции `F` и `G` "связанны": их вызовы моделируют рекурсивное передвижение по полному бинарному дереву высоты h - вызовы при переходе в левого / правого потомка при обходев глубину.
+            Путь до текущего узла дерева хранится в переменной `position`.
+
+            Каждый вызов  `F()` / `G()` вызывает внутреннюю функицию передвижения в следующую вершину: `transition_to(0)` / `transition_to(1)`.
+            При этом `transition_to` после обновления позиции дополнительно вызывает функцию `share(position)`.  
+          
+             Args:
+                h:     Высота полного бинарного действия.
+                f:     Функция, ассоциируемая с переходом в  левого потомка.
+                g:     Функция, ассоциируемая с переходом в правого потомка.
+                share: Функция вызываемая при переходе в новую вершину (каждом вызове `F`, `G`), принмающая путь `position` до новой вершины.
+
+                в  получает текущий путь (позицию) при переходе.
+                A callable that receives the current path (position) when a transition occurs.
+            
+            Returns:
+                Функции с общим внутреним состоянием `F(x)` и `G(y)`, возвращающие значения `f(x)` и `g(y)` и внешним эффектом в виде вызова `share(position)`.
+            """
+
+            position: List[bool] = []
+
+
+            def transition_to(which: bool) -> None:
+                """
+                Переходит в левого (`which == 0`) или в правого (`which == 1`) потомка.
+                При этом, если текщее положение это лист дерева, то сначала выполняется подъём вверх по дереву, до первого подъёма по левому ребру.
+                """
+                nonlocal position
+                if( len(position) == h ):
+                    # удаляем до первого нуля включительно ( .pop() - удаляет из списка последний элемент и возвращает его )
+                    while( position.pop() ): pass
+
+                position.append( which )
+                share( position )  # никаких гетеров / сеттеров
+
+            def F(x: Any) -> int:
+                transition_to(0)
+                return f(x)
+
+            def G(x: Any) -> int:
+                transition_to(1)
+                return g(x)
+
+            return F, G
+
+
+        def recurseve_check(Tree: Dict[int, List[Any]], ExpectedTreeList: List[int], indx: int = 0, path: str = "") -> None:
+            """
+            Рекурсивная проверка значений в узлах дерева.
+
+            Args:
+                Tree:             Текущее поддерево для проверки.
+                ExpectedTreeList: Список ожидаемых значений в порядке обхода в ширину.
+                indx:             Нуль-индексированая позиция значения текущего узла в ExpectedTreeList.
+                path:             Путь до текущего узла, кодируется спуском от корня дерева:  `L` - переход   в  левого потомка, `R` - в правого.
+
+            Raises:
+                AssertionError: "Error in position: {path}" - путь до первого несопадения в рекурсивном обходе.
+            """
+            (root_value, subTree), = Tree.items()
+            self.assertEqual(root_value, ExpectedTreeList[indx], "Error in position: " + path)
+
+            for i in range(len(subTree)):
+                recurseve_check(subTree[i], ExpectedTreeList, 2*indx + i + 1, path + " " + ("LR"[i]))
+
+
+        for h in cheked_height:
+            L = 2**(h+1) - 1           # Общее количество вершин в дереве высоты h.
+            TreeList = list(range(L))  # Установка ожидаемых значений в порядке обхода в ширину.
+
+            # Функция `pre_f` возвращает значения из `TreeList` на основе внутреннего индекса.
+            # Функция `update_f(position)` делает этот внутрениий индекс равным: `path_to_indx(position)`.
+            # Идейно последовательность действий для работы такая:
+            #   1. Переходим в узел "L R L L": `update_f([0, 1, 0, 0])`
+            #   2. Получаем значения этого узла из TreeList: `pref_f(Any)`
+            pre_f, update_f, is_ended_f = self.presetFunction(TreeList, lambda i, p: path_to_indx(p))
+
+            root_value = pre_f(0)
+
+            Tree = gen_bin_tree(h, root_value, *FG_positioned(h, pre_f, pre_f, update_f))
+            self.assertTrue(is_ended_f(), "Not all TreeList value's used in generation")
+
+            recurseve_check(Tree, TreeList)
+
+
+
+# Запуск тестов в Google Colab
+unittest.main(argv=[''], verbosity=2, exit=False)
