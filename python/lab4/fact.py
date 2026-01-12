58d430f12c0c8e70b24ed570a1ecc3d2cc011f5d
rfldxx <166624946+rfldxx@users.noreply.github.com>
2026-01-12 16:46:50 +0300

    ещё корректировка

--- python/lab4/fact.py
+++ python/lab4/fact.py
@@ -1,4 +1,3 @@
-# Battle Royale
 # Battle Royale факториалов
 
 import sys
@@ -13,15 +12,18 @@ sys.set_int_max_str_digits(100000)
 def fact_recursive(n):
     return n * fact_recursive(n-1) if n > 1 else 1
 
-  
-def fact_classic(n):
-    fact = 1
-    for x in range(1, n+1):
-        fact *= x
-    return fact
+
+def first_pair_merging(A):
+    result = 1
+    for x in A:
+        result *= x
+    return result
 
 
+import types
 def adjacent_pair_merging(A):
+    if( not isinstance(A, list) ): A = list(A)
+
     n = len(A)
     A.append(1)
     while n > 1:
@@ -31,19 +33,39 @@ def adjacent_pair_merging(A):
             A[i] = A[2*i]*A[2*i+1]
     return  A[0]
 
+import heapq
+def min_pair_merging(A):
+    if( not isinstance(A, list) ): A = list(A)
+    
+    n = len(A)
+    heapq.heapify(A)
+
+    for iteration in range(n-1):
+        mlt = heapq.heappop(A) * heapq.heappop(A)
+        heapq.heappush(A, mlt)
+    return A[0]
+
+
+
+def fact_classic(n, merging = first_pair_merging):
+    return merging( range(1, n+1) )
+
+
+
+# я слышал, что для ускорения вычислений, лучше перемножать максимально близкие по размеру числа
+
+
 
 from random import shuffle
-def fact_classic_1(n, pre_shuffle=1):
+def fact_classic_1(n, pre_shuffle=1, merging = first_pair_merging):
     A = list(range(1, n+1))
     if( pre_shuffle ): shuffle(A)
-    return adjacent_pair_merging(A)
+    return merging(A)
 
 
 
 from math import inf
 from collections import deque
-
-# я слышал, что для ускорения вычислений, лучше перемножать максимально близкие по размеру числа
 
 def fact3(n):
     # грубое разделение на две области:
@@ -69,6 +91,7 @@ def fact3(n):
 
 def fact3_1(n):
     # Как fact3, только вместо самих чисел, сравниваются их логарифмы
+    # (это не дало почти никакого прироста - действительно, наверное у больших чисел навряд ли совпадают разрядности и питон видит что у одного числа больше разрядов и не начинает их сравнивать посимвольно).
     A = deque()
     B = [(x, x.bit_length()) for x in range(n, 0, -1)]
     B.insert(0, (inf, inf)) # чтобы проще c if-ами было
@@ -107,25 +130,17 @@ def fact3_2(n, f):
 
         B.append( (new_mlt, digits) )
 
-
     return B[-1][0]
 
 
 
 import heapq
-def fact5(n):
+def fact5(n, merging = min_pair_merging):
     # Достаем два наименьших числа и возвращаемрезультат их произведения (!а что если читывать повторения чисел!)
-
-    heap = [x for x in range(1, n+1)]
-    heapq.heapify(heap)
-
-    for iteration in range(n-1):
-        mlt = heapq.heappop(heap) * heapq.heappop(heap)
-        heapq.heappush(heap, mlt)
-    return heap[0]
+    return merging( range(1, n+1) )
 
 
-def fact6(n):
+def fact6(n, merging = min_pair_merging):
     # Расматривается разложение n! на просте числа
     # Для каждого простого числа находится частота его вхождения и возведение в эту степень с помощью быстого возведения встепень
     # Затем последовательно перемножение степеней простых чисел
@@ -143,17 +158,10 @@ def fact6(n):
         
         pc.append((x,cnt))
 
-    heap = [pow(x, c) for (x,c) in pc]
-    heapq.heapify(heap)
-    L = len(pc)
-
-    for iteration in range(L-1):
-        mlt = heapq.heappop(heap) * heapq.heappop(heap)
-        heapq.heappush(heap, mlt)
-    return heap[0]
+    return min_pair_merging( pow(x, c) for (x,c) in pc )
 
 
-def fact6_1(n):
+def fact6_1(n, merging = min_pair_merging, merging_in_blocks = adjacent_pair_merging):
     # Подобно fact6, только сначала перемножаем все простые числа которые входят одинаковое число раз и потом возводим это произведение в нужную степень.
     sieve = [0]*(n+1)
 
@@ -172,20 +180,11 @@ def fact6_1(n):
         pc[cnt].append(x)
 
     heap = []
-    for (k, A) in enumerate(pc):
+    for (cnt, A) in enumerate(pc):
         if A != []:
-            heap.append( pow(adjacent_pair_merging(A), k) )
+            heap.append( pow(merging_in_blocks(A), cnt) )
 
-    heapq.heapify(heap)
-    L = len(heap)
-
-    for iteration in range(L-1):
-        mlt = heapq.heappop(heap) * heapq.heappop(heap)
-        heapq.heappush(heap, mlt)
-    return heap[0]
-
-
-
+    return merging( heap )
 
 
 
@@ -198,9 +197,8 @@ from functools import partial
 
 from math import factorial
 
-a = 20000
-check(factorial, a)                          № t = 0.011106155490015226 for val 20000
-
+a = 1000
+check(factorial, a)
 check(fact_recursive, a)                     # t = 0.11220435719000306  for val 20000
 
 check(fact_classic, a)                       # t = 0.10875383063999834  for val 20000
@@ -209,10 +207,10 @@ check(fact_classic_1, a)                     # t = 0.01967309137000484  for val 
 check(fact3, a)                              # t = 0.02237139875000139  for val 20000
 check(fact3_1, a)                            # t = 0.02788334269000188  for val 20000
 
-check(partial(fact4, f=lambda x: x//2), a)   # t = 0.03036437675000343  for val 20000
-check(partial(fact4, f=lambda x: x), a)      # t = 0.04168494952000401  for val 20000
-check(partial(fact4, f=lambda x: 3*x//2), a) # t = 0.030536252680012695 for val 20000
-check(partial(fact4, f=lambda x: 2*x), a)    # t = 0.030665910860006987 for val 20000
+check(partial(fact3_2, f=lambda x: x//2), a)   # t = 0.03036437675000343  for val 20000
+check(partial(fact3_2, f=lambda x: x), a)      # t = 0.04168494952000401  for val 20000
+check(partial(fact3_2, f=lambda x: 3*x//2), a) # t = 0.030536252680012695 for val 20000
+check(partial(fact3_2, f=lambda x: 2*x), a)    # t = 0.030665910860006987 for val 20000
 
 check(fact5, a)                              # t = 0.035707068629999415 for val 20000
 check(fact6, a)                              # t = 0.028582149750000098 for val 20000
